9df7bd1a0f5bffdaa682e1972980c040008aa2e0
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-16 13:25:02 +0530

    Update stop_wallpaper.bat

--- stop_wallpaper.bat
+++ stop_wallpaper.bat
@@ -1,8 +1,8 @@
 @echo off
-title Stop Luffy Live Wallpaper
-echo Stopping Luffy Live Wallpaper background process...
+title Stop Harry Potter Live Wallpaper
+echo Stopping Harry Potter Live Wallpaper background process...
 python "c:\Users\krishna\Downloads\Sceensaver\live_wallpaper.py" /stop
 echo.
-echo Luffy Live Wallpaper has been stopped.
+echo Harry Potter Live Wallpaper has been stopped.
 echo.
 pause
