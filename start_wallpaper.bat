fe97cbef7f8de7dab959c2daea5bf5542996558a
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-16 13:24:56 +0530

    Update start_wallpaper.bat

--- start_wallpaper.bat
+++ start_wallpaper.bat
@@ -1,8 +1,8 @@
 @echo off
-title Start Luffy Live Wallpaper
-echo Starting Luffy Live Wallpaper in the background...
+title Start Harry Potter Live Wallpaper
+echo Starting Harry Potter Live Wallpaper in the background...
 start "" pythonw "c:\Users\krishna\Downloads\Sceensaver\live_wallpaper.py" /start
 echo.
-echo Luffy Live Wallpaper is now running in the background and has been added to your Windows Startup folder.
+echo Harry Potter Live Wallpaper is now running in the background and has been added to your Windows Startup folder.
 echo.
 pause
